8f13a0e7b212c996eab3c043652a339d9873c942
Default <fernando_souza_vasconcelos@hotmail.com>
2020-02-12 20:49:22 -0300

    GaussSimples Rodando

--- GausSimples.py
+++ GausSimples.py
@@ -1,45 +1,45 @@
 def main():
-  x = []
-  val = 0
-  tam = 0
-  vetResp = []
-  print("A matriz é quadrada, porém deve ser inicializada com o valor de j+1")
-  tamI = int(input("Tamanho de i: "))
-  tamJ = int(input("Tamanho de j: "))
+    x = []
+    val = 0
+    tam = 0
+    vetResp = []
+    print("A matriz é quadrada, porém deve ser inicializada com o valor de j+1")
+    tamI = int(input("Tamanho de i: "))
+    tamJ = int(input("Tamanho de j: "))
 
-  for i in range(tamI):
-    y = []
-    for j in range(tamJ):
-      val = int(input(""))
-      y.append(val)
-    vetResp.append(y[j])
-    del(y[j])
-    x.append(y)
+    for i in range(tamI):
+        y = []
+        for j in range(tamJ):
+            val = float(input(""))
+            y.append(val)
+        vetResp.append(y[j])
+        del(y[j])
+        x.append(y)
  
-  print("vetResp:")
-  print(vetResp)
-  print("vetX:")
-  print(x)
-  gausSimples(x, vetResp, tamI, tamJ)
+    print("vetResp:")
+    print(vetResp)
+    print("vetX:")
+    print(x)
+    gausSimples(x, vetResp, tamI, tamJ)
 
 
 def gausSimples(x, vetResp, tamI, tamJ):
-  Resp = []
-  xResp = 0
-  flag = 0
-  for i in range(tamI-1, -1, -1):
-    if (i == tamI-1):
-      xResp = vetResp[i] / x[i][i]
-      Resp.append(xResp)
-    elif (i == tamI-2):
-      xResp = (vetResp[i] - (x[i][i+1])*Resp[flag-1]) / x[i][i]
-      Resp.append(xResp)
-    elif (i == tamI-3):
-      xResp = ((vetResp[i] - (x[i][i+1])*Resp[flag-1]) - (x[i][i+2]*Resp[flag-2]))/ x[i][i]
-      Resp.append(xResp)
-    flag += 1
+    Resp = []
+    xResp = 0
+    for i in range(len(vetResp)):
+        Resp.append(0)
+    for i in range(tamI-1, -1, -1):
+        flag = 0
+        if (i == tamI-1):
+            xResp = (vetResp[i])/x[i][i]
+            Resp[i] = xResp
+        else:
+            for j in range(i+1, tamI):
+                flag = flag + x[i][j] * Resp[j]
+            xResp = (vetResp[i] - flag)/x[i][i]
 
-  print(Resp)
+            Resp[i] = xResp
+    print(Resp)
 
 
 
